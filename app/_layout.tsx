0c87bf20a4b21ed040bb9674b18bcf2fffea20b9
cherbobs <eva.cherbonnier974@gmail.com>
2026-01-28 17:51:51 +0100

    adding logo for header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,18 +3,34 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { Stack, useRouter } from "expo-router";
+import { usePathname, Stack, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
+import Logo from "@/components/Logo";
+import { View, StyleSheet } from "react-native";
+import { useEffect } from "react";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
+
+export const unstable_settings = {
+  initialRouteName: "index",
+};
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const router = useRouter();
+  const HEADER_OFFSET = 12;
+
+  const handleBack = () => {
+    if (router.canGoBack()) {
+      router.back();
+    } else {
+      router.replace("/"); // fallback vers l'accueil
+    }
+  };
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
@@ -24,24 +40,69 @@ export default function RootLayout() {
             name="questions"
             options={{
               headerShown: true,
-              title: "", // supprime fallback
-              headerLeft: () => (
-                <Pressable
-                  onPress={router.back}
-                  style={{ paddingHorizontal: 12, marginTop: 16 }}
+              headerTransparent: true,
+              headerShadowVisible: false,
+              headerTitleAlign: "center",
+
+              headerStyle: {
+                backgroundColor: colorScheme === "dark" ? "#121212" : "#fff",
+              },
+              header: () => (
+                <View
+                  style={{
+                    height:120,
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems:"flex-end",
+                    paddingHorizontal: 12,
+                    paddingVertical: 10,
+                  }}
                 >
-                  <Ionicons
-                    name="chevron-back-outline"
-                    size={32}
+                  <Pressable onPress={handleBack} style={{ marginRight: 12 }}>
+                    <Ionicons
+                      name="chevron-back-outline"
+                      size={32}
+                      color={colorScheme === "dark" ? "white" : "black"}
+                    />
+                  </Pressable>
+
+                  <View
+                    style={{
+
+                      flex: 1,
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent:"flex-end",
+                    }}
+                  >
+                    <Logo
+                      width={80}
+                      height={50}
+                      color={colorScheme === "dark" ? "white" : "black"}
+                    />
+                  </View>
+                </View>
+              ),
+              /* headerLeft: () => (
+                <View style={{ marginTop: HEADER_OFFSET }}>
+                  <Pressable onPress={handleBack}>
+                    <Ionicons
+                      name="chevron-back-outline"
+                      size={32}
+                      color={colorScheme === "dark" ? "white" : "black"}
+                    />
+                  </Pressable>
+                </View>
+              ),
+              headerTitle: () => (
+                <View style={{ marginTop: HEADER_OFFSET }}>
+                  <Logo
+                    width={96}
+                    height={96}
                     color={colorScheme === "dark" ? "white" : "black"}
                   />
-                </Pressable>
-              ),
-              headerTransparent: true,
-              headerStyle: {
-                backgroundColor: colorScheme === "dark" ? "#121212" : "#fff", // couleur du header
-              },
-              headerShadowVisible: false, // supprime l'ombre / la ligne
+                </View>
+              ), */
             }}
           />
         </Stack>
